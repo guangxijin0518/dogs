bf5cfb7277e88de482c990edadba4c321b88be9d
guangxijin0518 <guangxijin0518@gmail.com>
2023-08-07 01:24:45 -0500

    added ListViewModel and updated ListFragment to use it

--- app/src/main/java/com/gjwork/dogs/view/ListFragment.kt
+++ app/src/main/java/com/gjwork/dogs/view/ListFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gjwork.dogs.R
 import com.gjwork.dogs.databinding.FragmentListBinding
+import com.gjwork.dogs.viewmodel.ListViewModel
 
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
 
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: ListViewModel
+    private val dogsListAdapter = DogsListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +28,46 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this)[ListViewModel::class.java]
+        viewModel.refresh()
+
+        binding.dogsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = dogsListAdapter
+        }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.dogs.observe(this, Observer { dogs ->
+            dogs?.let {
+                binding.dogsList.visibility = View.VISIBLE
+                dogsListAdapter.updateDogList(dogs)
+            }
+        })
+
+        viewModel.isError.observe(this, Observer { isError ->
+            isError?.let {
+                binding.listError.visibility = if (isError) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.isLoading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                binding.loadingView.visibility = if (isLoading) View.VISIBLE else View.GONE
+
+                if (isLoading) {
+                    binding.dogsList.visibility = View.GONE
+                    binding.listError.visibility = View.GONE
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
