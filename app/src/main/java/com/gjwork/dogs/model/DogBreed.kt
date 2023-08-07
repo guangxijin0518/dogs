91d21360cbc43712b31b17bc72b0ee4fd938b343
guangxijin0518 <guangxijin0518@gmail.com>
2023-08-07 00:06:52 -0500

    added dog item layout, DogBreed model and DogsListAdapter

---
+++ app/src/main/java/com/gjwork/dogs/model/DogBreed.kt
@@ -0,0 +1,11 @@
+package com.gjwork.dogs.model
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBread: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
---
+++ app/src/main/java/com/gjwork/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,36 @@
+package com.gjwork.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gjwork.dogs.R
+import com.gjwork.dogs.databinding.ItemDogBinding
+import com.gjwork.dogs.model.DogBreed
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogsViewHolder>() {
+    inner class DogsViewHolder(val binding: ItemDogBinding): RecyclerView.ViewHolder(binding.root)
+
+    fun updateDogList(newDogsList: List<DogBreed>) {
+        dogsList.clear()
+        dogsList.addAll(newDogsList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemDogBinding.inflate(inflater, parent, false)
+        return DogsViewHolder(binding)
+    }
+
+    override fun getItemCount() = dogsList.size
+
+    override fun onBindViewHolder(holder: DogsViewHolder, position: Int) {
+        with (holder) {
+            with (dogsList[position]) {
+                binding.name.text = this.dogBread
+                binding.lifespan.text = this.lifeSpan
+            }
+        }
+    }
+}
