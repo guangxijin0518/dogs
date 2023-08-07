b037a42ba02a062909fdbbb178068f1f60b6abc3
guangxijin0518 <guangxijin0518@gmail.com>
2023-08-07 02:29:34 -0500

    set up Retrofit

--- app/src/main/java/com/gjwork/dogs/model/DogBreed.kt
+++ app/src/main/java/com/gjwork/dogs/model/DogBreed.kt
@@ -1,11 +1,26 @@
 package com.gjwork.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("name")
     val dogBread: String?,
+
+    @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @SerializedName("bred_for")
     val bredFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 )
